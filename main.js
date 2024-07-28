501383a13547437b14e1b734d6d1963d5c7f2190
Romina-dll <romina_hajizadeh@yahoo.com>
2024-07-28 18:41:52 +0330

    dynamic feature changes

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ async function wait() {
 
     const weatherPromis = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherdata = await weatherPromis.json()
-    console.log(weatherdata.properties.periods[0].temperature)
+    const ourtemperature = weatherdata.properties.periods[0].temperature
+
+    document.querySelector("#temperature-output").textContent = ourtemperature
 
 
 
