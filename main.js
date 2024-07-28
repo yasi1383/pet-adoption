cd8a701d02f64b0f6b9374f7f485b6c752e255e1
Romina-dll <romina_hajizadeh@yahoo.com>
2024-07-28 19:05:37 +0330

    set name for id

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ async function wait() {
     const weatherPromis = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherdata = await weatherPromis.json()
     const ourtemperature = weatherdata.properties.periods[0].temperature
-
+    console.log(ourtemperature)
     document.querySelector("#temperature-output").textContent = ourtemperature
 
 
