98200878b0581a0ee0220780f5fe825183719335
Romina-dll <romina_hajizadeh@yahoo.com>
2024-07-28 20:28:27 +0330

    working in progress

--- main.js
+++ main.js
@@ -10,4 +10,13 @@ async function wait() {
 
 }
 wait()
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach((pet) => {
+        console.log(pet.name)
+    })
+
+}
+petsArea()
 
