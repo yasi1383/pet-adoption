5aeaecc9f8a11506a43facd5fb0c9632813a4125
Romina-dll <romina_hajizadeh@yahoo.com>
2024-07-29 09:13:46 +0330

    nearly finished pet card features

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+
 async function wait() {
 
 
@@ -14,9 +18,25 @@ async function petsArea() {
     const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
     petsData.forEach((pet) => {
-        console.log(pet.name)
-    })
+        const clone = template.content.cloneNode(true)
+        clone.querySelector("h1").textContent = pet.name
+        clone.querySelector(".pet-description").textContent = pet.description
+        clone.querySelector(".pet-age").textContent = currentAgeText(pet.birthYear)
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name} `
 
+        wrapper.appendChild(clone)
+    })
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 petsArea()
+function currentAgeText(birthYear) {
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - birthYear
+
+    if (age == 1) return "one year old"
+    if (age == 0) return "leas than one a year old"
+
+    return age + " years old"
+}
 
