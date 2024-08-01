6918a8e0f76bdd1cd3397713cb312fd52c508ced
Romina-dll <romina_hajizadeh@yahoo.com>
2024-08-01 09:39:09 +0330

    pet filter

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ async function petsArea() {
     const petsData = await petsPromise.json()
     petsData.forEach((pet) => {
         const clone = template.content.cloneNode(true)
+        clone.querySelector(".pet-card").dataset.species = pet.species
         clone.querySelector("h1").textContent = pet.name
         clone.querySelector(".pet-description").textContent = pet.description
         clone.querySelector(".pet-age").textContent = currentAgeText(pet.birthYear)
@@ -52,6 +53,17 @@ function handleButtonClick(e) {
         elment.classList.remove("active")
         //add active class
         e.target.classList.add("active")
+        //show picture of every species that selected
+        const currentFilter = e.target.dataset.filter
+        document.querySelectorAll(".pet-card").forEach(el => {
+            if (currentFilter == el.dataset.species || currentFilter == "all") {
+                el.style.display = "grid"
+            }
+            else {
+                el.style.display = "none"
+            }
+        })
+
     })
 
 }
